7407ea2d8159178c8d6603d8b3007b8349d9a8c5
dalilarichi9-cmd <dalilarichi9@gmail.com>
2026-09-14 16:07:56 +0200

    Update Keep_alive.py

--- Keep_alive.py
+++ Keep_alive.py
@@ -1,1 +1,19 @@
+# keep_alive.py
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "¡El bot de fútbol está vivo y funcionando!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
