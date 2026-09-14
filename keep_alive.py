2dc6f4661d74796b0fcfc1b11e8b4319693dee1c
dalilarichi9-cmd <dalilarichi9@gmail.com>
2026-09-14 16:51:02 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,19 +1,20 @@
-# keep_alive.py
 from flask import Flask
 from threading import Thread
+from waitress import serve
 import os
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "¡El bot de fútbol está vivo y funcionando!"
+    return "¡El bot de fútbol está vivo y optimizado para producción!"
 
 def run():
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    # Detecta el puerto de Render de forma dinámica
+    port = int(os.environ.get("PORT", 10000))
+    # Usamos serve() de waitress para eliminar el Warning de desarrollo
+    serve(app, host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
-
